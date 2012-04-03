ed0e8b881541daf9354c86f552c0248909167ca0
Patrik Stutz <patrik.stutz@gmail.com>
2012-04-03 08:24:37 -0500

    improved http support

--- examples/http_route_b.js
+++ examples/http_route_b.js
@@ -1,11 +1,10 @@
 var http = require("http");
 var anc = require("ancillary");
 
-var server = anc.createServer(function(c,data){
-    http._connectionListener.call(server,c);
-    c.ondata(new Buffer(data.header,"binary"),0,data.header.length);
-});
+var server = anc.createServer();
+
 server.on("request",function(req,res){
     res.end("B: "+req.url);
 });
+server.enableHttp();
 server.listen("/ancillary/b");
--- examples/http_router.js
+++ examples/http_router.js
@@ -4,19 +4,14 @@ var cp = require("child_process");
 
 http.createServer(function(req,res){
     req.connection.pause();
-    var d = req.method+" "+req.url+" HTTP/"+req.httpVersion+"\r\n";
-    for(var h in req.headers){
-        d+=h+": "+req.headers[h]+"\r\n";
-    }
-    d+= "\r\n";
     
     if(req.url.indexOf("/a/") == 0){
-        anc.send("/ancillary/a",req.connection,{type:"HTTP",header:d});
+        anc.send("/ancillary/a",req);
     }else if(req.url.indexOf("/b/") == 0){
-        anc.send("/ancillary/b",req.connection,{type:"HTTP",header:d});
+        anc.send("/ancillary/b",req);
     }else{
         res.end("<a href='/a/myfile.pdf'>Route A</a><br><a href='/b/myservice'>Route B</a>");
-    }  
+    }
     
     
 }).listen(1234);
