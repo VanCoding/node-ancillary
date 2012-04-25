8421a6243bc7387fa316424d8889d1eb11bbf21f
Patrik Stutz <patrik.stutz@gmail.com>
2012-04-25 07:07:28 -0500

    updated HTTP examples to new api

--- examples/http_route_a.js
+++ examples/http_route_a.js
@@ -1,11 +1,6 @@
 var http = require("http");
 var anc = require("ancillary");
 
-var server = anc.createServer();
-
-server.on("request",function(req,res){
-    res._last = true;
+var server = anc.createHttpServer(function(req,res){
     res.end("A: "+req.url);
-});
-server.enableHttp();
-server.listen("/ancillary/a");
+}).listen("/ancillary/a");
